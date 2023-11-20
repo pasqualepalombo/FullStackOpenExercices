1a21c4a45cc2eb08385aaf6dd61365d38c47c5f2
Pasquale <pasqualepalombo@outlook.com>
2023-11-20 18:48:07 +0100

    Exercise 1.7 unicafe step2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,83 @@
 import { useState } from 'react'
-// exercise 1.6
-
-const Button = ({handleClick, text}) => {
-  <button onClick={console.log("click")}>{text}</button>
-}
+// exercise 1.7 unicafe step2
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allFeedbacks, setAllFeedbacks] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
+  const Positive = (props) => {
+    const good = props.props[0]
+    const allFeedbacks = props.props[1]
+    if (allFeedbacks>0) {
+      const updatedPositive = (good/allFeedbacks)
+      setPositive(updatedPositive)
+      return(
+        <p>Average: {positive*100}%</p>
+      )
+    }
+    else {
+      return(
+        <p>Positive : 0</p>
+      )
+    }
+  }
+
+  const Average = (props) => {
+    const good = props.props[0]
+    const bad = props.props[1]
+    const allFeedbacks = props.props[2]
+    if (allFeedbacks>0) {
+      const updatedAverage = ((good-bad)/allFeedbacks)
+      setAverage(updatedAverage)
+      return(
+        <p>Average: {average}</p>
+      )
+    }
+    else {
+      return(
+        <p>Average: 0</p>
+      )
+    }
+  }
+
+  const handleGood = () => {
+    const updatedGood = good + 1
+        setGood(updatedGood)
+        const updatedAllFeedbacks = allFeedbacks + 1
+        setAllFeedbacks(updatedAllFeedbacks)
+  }
+
+const handleNeutral = () => {
+  const updatedNeutral = neutral + 1
+        setNeutral(updatedNeutral)
+        const updatedAllFeedbacks = allFeedbacks + 1
+        setAllFeedbacks(updatedAllFeedbacks)
+}
+
+const handleBad = () => {
+  const updatedBad = bad + 1
+        setBad(updatedBad)
+        const updatedAllFeedbacks = allFeedbacks + 1
+        setAllFeedbacks(updatedAllFeedbacks)
+}
+
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={()=> {
-        const updatedGood = good + 1
-        setGood(updatedGood)
-      }}>Good</button>
-      <button onClick={()=> {
-        const updatedNeutral = neutral + 1
-        setNeutral(updatedNeutral)
-      }}>Neutral</button>
-      <button onClick={()=> {
-        const updatedBad = bad + 1
-        setBad(updatedBad)
-      }}>Bad</button>
+      <button onClick={handleGood}>Good</button>
+      <button onClick={handleNeutral}>Neutral</button>
+      <button onClick={handleBad}>Bad</button>
       <h2>Statistics</h2>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>Total of feedbacks: {allFeedbacks}</p>
+      <Average props={[good, bad, allFeedbacks]} />
+      <Positive props={[good, allFeedbacks]} />
     </div>
   )
 }
