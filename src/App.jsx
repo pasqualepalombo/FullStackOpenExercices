d6f56cfb1f2ac14d044b3fa46f95a14d0579eb00
Pasquale <pasqualepalombo@outlook.com>
2023-11-21 14:54:26 +0100

    Exercise 1.7 unicafe step2 fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,53 @@
-import { useState } from 'react'
+import { useState } from "react";
 // exercise 1.7 unicafe step2
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [allFeedbacks, setAllFeedbacks] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [allFeedbacks, setAllFeedbacks] = useState(0);
 
   const Positive = (props) => {
-    const good = props.props[0]
-    const allFeedbacks = props.props[1]
-    if (allFeedbacks>0) {
-      const updatedPositive = (good/allFeedbacks)
-      setPositive(updatedPositive)
-      return(
-        <p>Average: {positive*100}%</p>
-      )
+    const good = props.props[0];
+    const allFeedbacks = props.props[1];
+    let positive = 0;
+    if (allFeedbacks > 0) {
+      positive = good / allFeedbacks;
     }
-    else {
-      return(
-        <p>Positive : 0</p>
-      )
-    }
-  }
+    return <p>Average: {positive * 100}%</p>;
+  };
 
   const Average = (props) => {
-    const good = props.props[0]
-    const bad = props.props[1]
-    const allFeedbacks = props.props[2]
-    if (allFeedbacks>0) {
-      const updatedAverage = ((good-bad)/allFeedbacks)
-      setAverage(updatedAverage)
-      return(
-        <p>Average: {average}</p>
-      )
+    const good = props.props[0];
+    const bad = props.props[1];
+    const allFeedbacks = props.props[2];
+    let average = 0;
+    if (allFeedbacks > 0) {
+      average = (good - bad) / allFeedbacks;
     }
-    else {
-      return(
-        <p>Average: 0</p>
-      )
-    }
-  }
+    return <p>Average: {average}%</p>;
+  };
 
   const handleGood = () => {
-    const updatedGood = good + 1
-        setGood(updatedGood)
-        const updatedAllFeedbacks = allFeedbacks + 1
-        setAllFeedbacks(updatedAllFeedbacks)
-  }
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    const updatedAllFeedbacks = allFeedbacks + 1;
+    setAllFeedbacks(updatedAllFeedbacks);
+  };
 
-const handleNeutral = () => {
-  const updatedNeutral = neutral + 1
-        setNeutral(updatedNeutral)
-        const updatedAllFeedbacks = allFeedbacks + 1
-        setAllFeedbacks(updatedAllFeedbacks)
-}
+  const handleNeutral = () => {
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    const updatedAllFeedbacks = allFeedbacks + 1;
+    setAllFeedbacks(updatedAllFeedbacks);
+  };
 
-const handleBad = () => {
-  const updatedBad = bad + 1
-        setBad(updatedBad)
-        const updatedAllFeedbacks = allFeedbacks + 1
-        setAllFeedbacks(updatedAllFeedbacks)
-}
+  const handleBad = () => {
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    const updatedAllFeedbacks = allFeedbacks + 1;
+    setAllFeedbacks(updatedAllFeedbacks);
+  };
 
   return (
     <div>
@@ -79,7 +63,7 @@ const handleBad = () => {
       <Average props={[good, bad, allFeedbacks]} />
       <Positive props={[good, allFeedbacks]} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
