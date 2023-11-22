7398899e651c1d7ab7396d06704c4a56fcab7b7d
Pasquale <pasqualepalombo@outlook.com>
2023-11-22 18:53:39 +0100

    Exercise 1.12 anecdotes step1

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,32 @@
 import { useState } from 'react'
-// exercise 1.11 unicafe step6
+// exercise 1.12 anecdotes step1
 
-const Statistics = (props) => {
-  const sum = props.good + props.neutral + props.bad
-  if (sum === 0){
-    return (
-      <div>
-        <p>No feedback given</p>
-      </div>
-    )
-  } 
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const randomNumber = () => {
+    let random = Math.floor(Math.random()*anecdotes.length)
+    setSelected(random)
+  }
+
   return (
-    <table>
-      <tbody>
-        <StatisticLine text='good' value={props.good} />
-        <StatisticLine text='neutral' value={props.neutral} />
-        <StatisticLine text='bad' value={props.bad} />
-        <StatisticLine text='all' value={sum} />
-        <StatisticLine text='average' value={(props.good * 1 + props.neutral * 0 + props.bad * (-1))/sum} />
-        <StatisticLine text='positive' value={`${parseFloat(props.good / sum) * 100 } %`}/>
-      </tbody>
-    </table>
+    <div>
+      <button onClick={randomNumber}>Random</button>
+      {anecdotes[selected]}
+    </div>
   )
 }
 
-const StatisticLine = (props) => {
-return (
-  <tr>
-    <td>{props.text}</td> 
-    <td>{props.value}</td>
-  </tr>
-)
-}
-
-const Button = (props) => {
-return (
-  <div>
-    <button onClick={props.handleClick} >{props.text}</button>
-  </div>
-)
-}
-
-
-const App = () => {
-// save clicks of each button to its own state
-const [good, setGood] = useState(0)
-const [neutral, setNeutral] = useState(0)
-const [bad, setBad] = useState(0)
-
-const handleGoodClick = () => {
-  setGood(good + 1)
-}
-
-const handleNeutralClick = () => {
-  setNeutral(neutral + 1)
-}
-
-const handleBadClick = () => {
-  setBad(bad + 1)
-}
-
-return (
-  <div>
-      <h1>give feedback</h1>
-      <Button handleClick={handleGoodClick} text='good'/>
-      <Button handleClick={handleNeutralClick} text='neutral'/>
-      <Button handleClick={handleBadClick} text='bad'/>
-      <h1>statistics</h1>
-      <Statistics good={good} neutral={neutral} bad={bad} />
-  </div>
-)
-}
 
 export default App
