200bc6db3412de3a67087b991c65de751d30380b
Pasquale <pasqualepalombo@outlook.com>
2023-11-22 17:17:58 +0100

    Exercise 1.10 unicafe step5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
-// exercise 1.7 unicafe step2
+// exercise 1.10 unicafe step5
+
+const StatisticLine = ({text, value}) =>{
+  return(
+    <p>{text}:{value}</p>
+  )
+}
 
 const Statistics = (props) => {
   const good = props.props[0]
@@ -14,9 +20,9 @@ const Statistics = (props) => {
     return(
       <div>
         <h2>Statistics</h2>
-        <p>Good: {good}</p>
-        <p>Neutral: {neutral}</p>
-        <p>Bad: {bad}</p>
+        <StatisticLine text = "Good" value={good}/>
+        <StatisticLine text = "Neutral" value={neutral}/>
+        <StatisticLine text = "Bad" value={bad}/>
         <p>Total of feedbacks: {allFeedbacks}</p>
         <p>Average: {average}%</p>
         <p>Positive: {positive}%</p>
@@ -35,32 +41,38 @@ const Statistics = (props) => {
   )
 }
 
+const Button = ({text, handleFunction}) => {
+  return (
+      <button onClick={handleFunction}>{text}</button>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleGood = () => {
+const handleGood = () => {
     const updatedGood = good + 1
-        setGood(updatedGood)
-  }
+    setGood(updatedGood)
+  } 
 
 const handleNeutral = () => {
   const updatedNeutral = neutral + 1
-        setNeutral(updatedNeutral)
+    setNeutral(updatedNeutral)
 }
 
 const handleBad = () => {
   const updatedBad = bad + 1
-        setBad(updatedBad)
+    setBad(updatedBad)
 }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handleGood}>Good</button>
-      <button onClick={handleNeutral}>Neutral</button>
-      <button onClick={handleBad}>Bad</button>
+      <Button text ='Good' handleFunction={handleGood}/>
+      <Button text ='Neutral' handleFunction={handleNeutral}/>
+      <Button text ='Bad' handleFunction={handleBad}/>
       <Statistics props={[good, neutral, bad]} />
     </div>
   )
