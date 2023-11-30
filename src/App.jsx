44de24c63a13854d3fe5d11aa5a8f9ee53a5cdc4
Pasquale <pasqualepalombo@outlook.com>
2023-11-30 17:30:57 +0100

    Exercise 2.1 information step 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,49 @@
 import { useState } from 'react'
-// exercise 1.14 anecdotes step3
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const [voted, setVoted] = useState(Array(anecdotes.length).fill(0))
+// Exercise 2.1 information step 6
 
-  const randomNumber = () => {
-    let random = Math.floor(Math.random()*anecdotes.length)
-    setSelected(random)
-  }
+const Part = ({part}) => {
+  return (
+    <li>{part.name}</li>
+  )
+}
 
-  const voteMe = () => {
-    const newVotes = [...voted]
-    newVotes[selected] += 1
-    setVoted(newVotes)
-  }
-
-  const ReturnMaxValue = () => {
-    const maxValue = Math.max(...voted)
-    let value = 0
-    while (voted.length) {
-      if (voted[value]==maxValue) break
-      value += 1
-    }
-    return (
-      <p>{anecdotes[value]}</p>
-    )
-
-  }
-
+const Course = ({course}) => {
+  console.log(course.parts)
+  var parts = course.parts
   return (
     <div>
-      <h1>Anecdote of the day:</h1>
-      <p>{anecdotes[selected]}</p>
-      <p>This Anecdotes has: {voted[selected]}votes!</p>
-      <button onClick={voteMe}>Vote me!</button>
-      <button onClick={randomNumber}>Next Anecdote</button>
-      <h2>Most voted Anecdote:</h2>
-      <ReturnMaxValue />
+      <h1>{course.name}</h1>
+      <ul>
+        {parts.map(part => <Part key={part.id} part={part} />)}
+      </ul>
     </div>
   )
 }
 
+function App() {
+  const course = {
+    id: 1,
+    name:'Half Stack Application Development',
+    parts: [
+    {
+    id:1,
+    name:'Fundamentals of React',
+    exercises:10,
+    },
+    {
+    id:2,
+    name:'Using props to pass data',
+    exercises:7,
+    },
+    {
+    id:3,
+    name:'State of a component',
+    exercises:14,
+    },
+    ]
+  }
+  return <Course course={course}/>
+}
 
 export default App
